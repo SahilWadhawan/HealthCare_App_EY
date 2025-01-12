741cc94bbc4aa9855408ce922b7dca2fa5978602
Sahil <122047068+SahilWadhawan@users.noreply.github.com>
2025-01-12 23:01:18 +0530

    Create Model.dart

---
+++ QRscanner/Model.dart
@@ -0,0 +1,17 @@
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'q_rscanner_widget.dart' show QRscannerWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class QRscannerModel extends FlutterFlowModel<QRscannerWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
